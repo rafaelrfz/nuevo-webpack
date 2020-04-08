74e899cc23412172cd037139aa968dc5c22885cc
Rafael Florez <rafflorez7@gmail.com>
2020-04-08 17:20:42 -0500

    Subiendo archivos

---
+++ src/mi-modulo.js
@@ -0,0 +1,15 @@
+export function saludar (nombre){
+    return `Hola ${nombre}`;
+}
+
+export function despedir (nombre){
+    return `Hasta ${luego()} ${nombre}`;
+}
+
+//var x = 777;
+
+export var x = 777;
+
+function luego(){
+    return 'luego';
+}
---
+++ src/principal.js
@@ -0,0 +1,19 @@
+//Rename named imports
+import {saludar as saludo, despedir} from './mi-modulo.js'
+
+console.log(saludo('Pepito'));
+console.log(despedir('Pepa'));
+
+// Named imports
+//import {saludar, despedir} from './mi-modulo.js'
+/*console.log(saludar('Juan'));
+console.log(despedir('Juan'));
+
+// Namespace import 
+//import * as myModule from './mi-modulo.js'
+
+
+
+console.log(myModule.saludar('Juan'));
+console.log(myModule.despedir('Juan'));
+console.log(myModule.x);*/
